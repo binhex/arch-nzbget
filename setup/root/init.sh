d0979f431b5bbdcb4003618f03319efb2cd92865
binhex <megalith01@gmail.com>
2016-09-11 22:23:04 +0100

    config supervisor for pid 1

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775 /usr/bin/nzbget /usr/share/nzbget/nzbget.conf /home/nobody
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
