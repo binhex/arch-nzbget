6cea75ce10c65dfc81b581ca7b8c63e973435f2c
binhex <megalith01@gmail.com>
2016-10-07 13:32:26 +0100

    silence stdout, manually create /data/dst folder for log

--- setup/nobody/start.sh
+++ setup/nobody/start.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# check if nzbget.conf exists, if not copy sample config
+# check if nzbget.conf exists, if not copy default config
 if [[ -f /config/nzbget.conf ]]; then
 
-	echo "nzbget.conf exists"
-	
+	echo "[info] NZBGet configuration file exists"
+
 else
-	
+
+	echo "[info] NZBGet configuration does not exist, copying default configuration file to /config/..."
+
 	# copy to /config
 	cp /usr/share/nzbget/nzbget.conf /config/
 
 	# set maindir to /data folder for downloads
 	sed -i 's/MainDir=~\/downloads/MainDir=\/data/g' /config/nzbget.conf
-	
+
 fi
 
-# start nzbget non-daemonised and specify config file
-/usr/bin/nzbget -c /config/nzbget.conf -s
+# need to create dst folder as its not auto generated (used to store nzb logs)
+mkdir -p /data/dst
+
+# start nzbget non-daemonised and specify config file (close stdout due to chatter)
+/usr/bin/nzbget -c /config/nzbget.conf -s 1>&-
