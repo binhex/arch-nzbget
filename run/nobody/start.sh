6b1aeb69cb7074a7c7521a94d9997beb692a13e2
binhex <megalith01@gmail.com>
2018-07-30 11:45:45 +0100

    add patch for ConfigTemplate

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -17,13 +17,10 @@ else
 
 fi
 
-# Due to the change in install location we need to patch the NZBGet configuration
-# file for the WebDir - triggered by switch from AOR package to NZBGet GitHub installer
-echo "[info] Patching NZBGet config file for WebDir location..."
+# Due to the change in install location (AOR to NZBGet installer) we need to patch the NZBGet configuration file
+echo "[info] Patching NZBGet config file for WebDir and ConfigTemplate locations..."
 sed -i -e 's~WebDir=/usr/share/webui~WebDir=${AppDir}/webui~g' /config/nzbget.conf
-
-# need to create dst folder as its not auto generated (used to store nzb logs)
-mkdir -p /data/dst
+sed -i -e 's~ConfigTemplate=/usr/share/nzbget/nzbget.conf~ConfigTemplate=${AppDir}/nzbget.conf~g' /config/nzbget.conf
 
 # start nzbget non-daemonised and specify config file (close stdout due to chatter)
 /usr/local/bin/nzbget/nzbget -c /config/nzbget.conf -s 1>&-
