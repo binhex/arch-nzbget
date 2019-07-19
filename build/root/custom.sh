c80e637fd391dc4d98eef90492504a0626bfd312
binhex <megalith01@gmail.com>
2019-07-19 14:29:29 +0100

    put system scripts on path

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -10,7 +10,7 @@ download_filename="nzbget.*bin-linux.run"
 ####
 
 # download nzbget binary installer
-/root/github.sh -df "${download_filename}" -dp "/tmp" -ep "" -ip "${install_path}" -go "nzbget" -gr "nzbget" -rt "binary"
+github.sh -df "${download_filename}" -dp "/tmp" -ep "" -ip "${install_path}" -go "nzbget" -gr "nzbget" -rt "binary"
 
 # run installer to install nzbget and dependencies
 cd "${install_path}" && ./nzbget*bin-linux.run --arch x86_64 --destdir .
