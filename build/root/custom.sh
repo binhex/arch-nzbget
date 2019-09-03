24fe686e30dea2833799c00b912070d9e41edb9c
binhex <megalith01@gmail.com>
2019-09-03 13:29:09 +0100

    another stab at fixing unpack issue

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -20,3 +20,13 @@ rm -f "${install_path}"/nzbget*bin-linux.run
 
 # remove statically built unrar as it looks buggy, see here https://forum.nzbget.net/viewtopic.php?f=3&t=3237
 rm -f /usr/local/bin/nzbget/unrar
+
+# install specific version of libunrar (5.6.3 - last known working version) to try and prevent 'stuck' operation on unpack
+package_name="libunrar.tar.xz"
+curly.sh -rc 6 -rw 10 -of "/tmp/${package_name}" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${package_name}"
+pacman -U "/tmp/${package_name}" --noconfirm
+
+# install specific version of unrar (5.6.3 - last known working version) to try and prevent 'stuck' operation on unpack
+package_name="unrar.tar.xz"
+curly.sh -rc 6 -rw 10 -of "/tmp/${package_name}" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${package_name}"
+pacman -U "/tmp/${package_name}" --noconfirm
