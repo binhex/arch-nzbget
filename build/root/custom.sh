0676b5634e08afea608759fa5bce582bf0f1e944
binhex <megalith01@gmail.com>
2023-11-06 09:02:39 +0000

    switch to nzbget-ng (#7)

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -6,25 +6,17 @@ set -e
 # github releases
 ####
 
-# download nzbget binary installer
-github.sh --install-path "/usr/local/bin/nzbget" --github-owner "nzbget" --github-repo "nzbget" --download-assets "nzbget.*bin-linux.run" --compile-src 'chmod +x nzbget*bin-linux.run && ./nzbget*bin-linux.run --arch x86_64 --destdir .'
+# create location to store clone source
+mkdir -p /tmp/src/nzbget
 
-# remove installer
-rm -f "/usr/local/bin/nzbget/nzbget*bin-linux.run"
+# clone from default branch (currently develop)
+git clone https://github.com/nzbget-ng/nzbget /tmp/src/nzbget
 
-# remove statically built unrar as it looks buggy, see here https://forum.nzbget.net/viewtopic.php?f=3&t=3237
-rm -f /usr/local/bin/nzbget/unrar
+# cd to clone location
+cd /tmp/src/nzbget
 
-# install specific version of libunrar (5.6.3 - last known working version) to try and prevent 'stuck' operation on unpack
-package_name="libunrar.tar.xz"
-rcurl.sh -o "/tmp/${package_name}" "https://github.com/binhex/packages/raw/master/compiled/${TARGETARCH}/${package_name}"
-pacman -U "/tmp/${package_name}" --noconfirm
+# run make to compile from source
+make
 
-# install specific version of unrar (5.6.3 - last known working version) to try and prevent 'stuck' operation on unpack
-package_name="unrar.tar.xz"
-rcurl.sh -o "/tmp/${package_name}" "https://github.com/binhex/packages/raw/master/compiled/${TARGETARCH}/${package_name}"
-pacman -U "/tmp/${package_name}" --noconfirm
-
-# openssl 1.0.x will fail the connection when it detects the expired certificate, replace with updated pem
-# see https://github.com/nzbget/nzbget/issues/784#issuecomment-931609658
-rcurl.sh -o "/usr/local/bin/nzbget/cacert.pem" -L "https://nzbget.net/info/cacert.pem"
+# install from compiled source
+make install
--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/dumb-init /bin/bash
 
+# define path to webdir and config template
+app_path='/usr/local/share/nzbget'
+
+# define path to config file copied to /config
+config_filepath='/config/nzbget.conf'
+
 # check if nzbget.conf exists, if not copy default config
 if [[ -f /config/nzbget.conf ]]; then
 
@@ -10,17 +16,20 @@ else
 	echo "[info] NZBGet configuration does not exist, copying default configuration file to /config/..."
 
 	# copy to /config
-	cp /usr/local/bin/nzbget/nzbget.conf /config/
+	cp "${app_path}/nzbget.conf" /config/
 
 	# set maindir to /data folder for downloads
-	sed -i 's/MainDir=~\/downloads/MainDir=\/data/g' /config/nzbget.conf
+	sed -i 's/^MainDir=.*/MainDir=\/data/g' "${config_filepath}"
 
 fi
 
-# Due to the change in install location (AOR to NZBGet installer) we need to patch the NZBGet configuration file
 echo "[info] Patching NZBGet config file for WebDir and ConfigTemplate locations..."
-sed -i -e 's~WebDir=/usr/share/nzbget/webui~WebDir=${AppDir}/webui~g' /config/nzbget.conf
-sed -i -e 's~ConfigTemplate=/usr/share/nzbget/nzbget.conf~ConfigTemplate=${AppDir}/nzbget.conf~g' /config/nzbget.conf
 
-# start nzbget non-daemonised and specify config file (close stdout due to chatter)
-/usr/local/bin/nzbget/nzbget --option UnrarCmd=/usr/bin/unrar -c /config/nzbget.conf -s 1>&-
+# set web path baed off app path
+sed -i -e "s~^WebDir=.*~WebDir=${app_path}/webui~g" "${config_filepath}"
+
+# set config template path based off app path
+sed -i -e "s~^ConfigTemplate=.*~ConfigTemplate=${app_path}/nzbget.conf~g" "${config_filepath}"
+
+echo "[info] Starting NZBGet non-daemonised and specify config file (close stdout due to chatter)..."
+/usr/local/bin/nzbget --option UnrarCmd=/usr/sbin/unrar -c "${config_filepath}" -s 1>&-
