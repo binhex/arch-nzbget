95da8d7ef8c23335938026a7747846b6dbac63f4
binhex <megalith01@gmail.com>
2025-04-01 22:34:24 +0100

    update 7zip package name

--- build/root/install.sh
+++ build/root/install.sh
@@ -39,7 +39,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
 ####
 
 # define pacman packages
-pacman_packages="git github-cli base-devel unrar libxml2 p7zip python par2cmdline"
+pacman_packages="git github-cli base-devel unrar libxml2 7zip python par2cmdline"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
